23bd327234f9b0c49bccadea2867ad78737b8977
gniht <jasonthomasjat@gmail.com>
2022-01-23 10:51:36 -0800

    added pug

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const fs = require("fs");
+const pug = require('pug');
 
 const app = express();
 const port = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+const compiledFunction = pug.compileFile('template.pug');
+console.log(compiledFunction({name: 'Timothy'}));
 
 app.use(express.static('views'));
 
@@ -10,10 +17,12 @@ app.listen(port, () =>{
   console.log(`example app listening at http://localhost:${port}`);
 });
 
-app.get()
-
 app.get('/', (req, res) => {
-  console.log(`request URL: ${req.url}`); 
-  res.end('index');  
+  res.render('index', { title: 'Hey', message: 'hihi msg'})
 });
-// hi
+
+// app.get('/', (req, res) => {
+//   console.log(`request URL: ${req.url}`); 
+//   res.end('index');  
+// });
+
