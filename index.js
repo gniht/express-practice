a2abed452dc8bc43847207b050a9ce23d6f63563
gniht <gniht@pop-os.localdomain>
2023-01-07 10:43:04 -0800

    retrieves member given id

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ const members = require('./Members.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
-app.get('/api/members', (req, res) => {
-  res.json(members);
-})
+// app.get('/', (req, res) => res.send('<h1>hihi</h1>') );
 
 // get static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/members/:id', (req, res) => {
+  let result;
+
+  members.forEach( member => {
+    if(member.id === parseInt(req.params.id)){
+    result =  member;
+  }});
+
+  if(result){
+    res.json(result);
+  }else{
+    res.status(400).json({ msg: `Error - no member with id: ${req.params.id} was found` });
+  }
+});
 
 app.listen(PORT, () =>{
   console.log(`example app listening ${PORT}`);
 });
 
 
-
-// app.get('/', (req, res) => {
-//   console.log(`request URL: ${req.url}`); 
-//   res.end('index');  
-// });
 
